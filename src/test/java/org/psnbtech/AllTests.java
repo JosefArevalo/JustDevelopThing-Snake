64b282adeeb2b9f73f64c79c2282f06431bbf7b4
Josef <91350457+JosefArevalo@users.noreply.github.com>
2022-12-04 13:41:47 -0800

    fix?

--- src/test/java/org/psnbtech/AllTests.java
+++ src/test/java/org/psnbtech/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ClockTest.class, SnakeGameTest.class, StartGame.class })
+@SuiteClasses({ ClockTest.class, SnakeGameTest.class })
 
 public class AllTests {
 	
